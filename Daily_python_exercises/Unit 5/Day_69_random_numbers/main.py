c0e35b3cce78d9af56f056a9286f789b2ad13ef0
Diego <diegofermend@gmail.com>
2026-04-26 19:44:28 +0200

    random numbers

--- Daily_python_exercises/Unit 5/Day_69_random_numbers/main.py
+++ Daily_python_exercises/Unit 5/Day_69_random_numbers/main.py
@@ -1,1 +1,30 @@
 import random
+
+print(f"Here is a random number between 0 and 1:\n{random.random()}")
+while True:
+    choice = input("Do you want to see another? Y or N\n")
+    if choice.capitalize() == "Y":
+        print(random.random())
+    else:
+        break
+
+num1 = float(input("Tell me two numbers\n"))
+num2 = float(input(""))
+print(f"Here is a random number between {num1} and {num2}:\n{random.uniform(num1, num2)}")
+while True:
+    choice = input("Do you want to see another? Y or N\n")
+    if choice.capitalize() == "Y":
+        print(random.uniform(num1, num2))
+    else:
+        break
+
+
+num1 = float(input("Tell me two numbers\n"))
+num2 = float(input(""))
+print(f"Here is a random number between {num1} and {num2}:\n{random.randint(num1, num2)}")
+while True:
+    choice = input("Do you want to see another? Y or N\n")
+    if choice.capitalize() == "Y":
+        print(random.randint(num1, num2))
+    else:
+        break
