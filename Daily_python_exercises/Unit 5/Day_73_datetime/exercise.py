a386f789288dc6e65b7bef62ed73983d76b63e1b
Diego <diegofermend@gmail.com>
2026-04-30 16:19:08 +0200

    datetime

--- Daily_python_exercises/Unit 5/Day_73_datetime/exercise.py
+++ Daily_python_exercises/Unit 5/Day_73_datetime/exercise.py
@@ -3,5 +3,6 @@ import datetime
 target1 = datetime.datetime(2027, 4, 12, 19, 24, 12)
 target2 = datetime.datetime(2025, 11, 9, 4, 57, 20)
 actual = datetime.datetime.now()
+
 print(f"{target1} is in the future") if target1>actual else print(f"{target1} is the past")
 print(f"{target2} is in the future") if target2>actual else print(f"{target2} is the past")
