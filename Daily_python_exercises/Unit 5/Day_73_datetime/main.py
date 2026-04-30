5d1035373cb40e7422cab05d56b3d12266ee55c3
Diego <diegofermend@gmail.com>
2026-04-30 16:18:30 +0200

    datetime

---
+++ Daily_python_exercises/Unit 5/Day_73_datetime/main.py
@@ -0,0 +1,13 @@
+import datetime
+
+date = datetime.date(2090,9,9)
+print(date)
+
+todays_date = datetime.date.today()
+print(todays_date)
+
+time = datetime.time(19,19,19)
+print(time)
+
+time_now = datetime.datetime.now()
+print(time_now)
