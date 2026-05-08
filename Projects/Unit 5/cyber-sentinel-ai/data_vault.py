1d0aa34ea933985087885f3914256a02cbfd00db
Diego <diegofermend@gmail.com>
2026-05-08 18:32:30 +0200

    cyber sentinel ai

--- Projects/Unit 5/cyber-sentinel-ai/data_vault.py
+++ Projects/Unit 5/cyber-sentinel-ai/data_vault.py
@@ -3,3 +3,8 @@ import random
 network_load = random.randint(0,100)
 
 sec_threats_cve = {"SQLi": 9.7,"XSS": 8.3, "Info disclosure": 5.3, "Banner Grabbing": 3.2 }
+
+security_policies = (
+    "PROTOCOL_01: Do not export sensitive biometric data.",
+    "PROTOCOL_02: AI core must remain isolated from public web.",
+    "PROTOCOL_03: Encryption is mandatory for all neural transfers.")
