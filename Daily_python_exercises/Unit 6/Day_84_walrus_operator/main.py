12d2a2a5b2dd32b114b3321a38a515a9573cd2b8
Diego <diegofermend@gmail.com>
2026-05-19 16:15:19 +0200

    walrus operator

---
+++ Daily_python_exercises/Unit 6/Day_84_walrus_operator/main.py
@@ -0,0 +1,9 @@
+word1 = "Orange"
+print(word1)
+
+print (word2:="Blue")
+print(word2)
+
+clothes = list()
+while clothe:=input("What clothe do you like\n") != "exit":
+    (clothes:=list()).append(clothe)
