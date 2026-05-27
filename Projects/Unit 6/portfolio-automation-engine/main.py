09394c15c901585e38fa8803eb5ff6d1b1a196e4
Diego <diegofermend@gmail.com>
2026-05-27 19:36:08 +0200

    portfolio automation engine

--- Projects/Unit 6/portfolio-automation-engine/main.py
+++ Projects/Unit 6/portfolio-automation-engine/main.py
@@ -6,7 +6,7 @@ import datetime
 while True:
     match selection:=input("Menu:\n 1)Run simulation\n 2)View Global analytics\n 3)Exit\n"):
         case "1":
-
+            stocks_names = ("NDX", "AAPL", "MSFT", "NYSE")
             @utils.show_time
             def simulate():
                 while True:
@@ -16,9 +16,16 @@ while True:
                         while True:
                             start_date = input("Enter your start date:\n")
                             if isinstance(utils.date_validator(start_date), datetime.date):
+                                start_date = utils.date_validator(start_date)
                                 break
 
-                        users_days = int(input("Enter the amount of days you want to simulate"))
+                        users_days = int(input("Enter the amount of days you want to simulate:\n"))
+
+                        for day, prices in enumerate(engine.prices_generator(stocks_names, users_days)):
+                            print(f"-- Day {day}:  --")
+                            #Hacer que aparezca aqui tambien la fecha de cada dia segun pasan
+                            for x in prices:
+                                print(f"- {x['name']}: {x['price']}")
 
                     except ValueError:
                         print("Error, you must enter a number for the initial capital and the number of days")
