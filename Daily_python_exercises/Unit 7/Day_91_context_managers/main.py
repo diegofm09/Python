0ae23bd7a37d676714c1c07ad1bbece52569fa6c
Diego <diegofermend@gmail.com>
2026-06-04 18:16:26 +0200

    context managers

--- Daily_python_exercises/Unit 7/Day_91_context_managers/main.py
+++ Daily_python_exercises/Unit 7/Day_91_context_managers/main.py
@@ -1,5 +1,6 @@
 with open("Daily_python_exercises/Unit 7/Day_91_context_managers/films.txt", "w") as file:
     file.write("Interestellar\nMidssomar\nHereditary\nInception\n")
 
+
 with open("Daily_python_exercises/Unit 7/Day_91_context_managers/films.txt", "a") as file:
     file.write("Shutter Island\nStar Wars\n")
