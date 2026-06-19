7bee9f5040848bbee4888a97468b7e5b52a67313
Diego <diegofermend@gmail.com>
2026-06-19 19:50:38 +0200

    json introduction

--- Daily_python_exercises/Unit 7/Day_101_json_introduction/main.py
+++ Daily_python_exercises/Unit 7/Day_101_json_introduction/main.py
@@ -4,3 +4,8 @@ with open("Daily_python_exercises/Unit 7/Day_101_json_introduction/products.json
     product = json.load(file)
 
 print(f'Product: {product["name"]}\nPrice: {product["price"]}\nIs it in stock?: {product["in_stock"]}')
+
+with open("Daily_python_exercises/Unit 7/Day_101_json_introduction/id.json", "r") as file2:
+    id = json.load(file2)
+
+print(f'Welcome {id["name"]}:\n -Age: {id["age"]}\n -Country: {id["country"]}\n -Money: {id["Money"]}$')
