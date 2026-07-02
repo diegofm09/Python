b5751f9f734b5046547d4514d71df50b485f2de5
Diego <diegofermend@gmail.com>
2026-07-02 13:08:28 +0200

    advanced reading practice

---
+++ Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice/main.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+main_path = Path(__file__).resolve().parent
+
+with open(main_path/"file.txt","w") as file:
+    file.write("El sol brilla\n")
+    file.write("La luna duerme\n")
+    file.write("Las estrellas observan\n")
+
+with open("Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice/file.txt", "r") as file:
+    print(file.readline().strip())
+    print(file.readline().strip())
+    print(file.tell())
+
+with open("Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice/file.txt", "r") as file:
+    lines = file.readlines()
+
+line_3 = lines[2].upper()
+print(line_3)
+
+with open("Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice/file.txt", "r") as file:
+    song = file.read()
+song = song.replace("luna", "selva")
+print(song)
