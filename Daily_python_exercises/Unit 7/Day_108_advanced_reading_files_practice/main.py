8359b0f4288a5d11caf701661b09b01ac0a38431
Diego <diegofermend@gmail.com>
2026-07-03 13:24:52 +0200

    advanced reading practice

--- Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice/main.py
+++ Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice/main.py
@@ -16,9 +16,12 @@ with open("Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice
     lines = file.readlines()
 
 line_3 = lines[2].upper()
+
 print(line_3)
 
 with open("Daily_python_exercises/Unit 7/Day_108_advanced_reading_files_practice/file.txt", "r") as file:
     song = file.read()
+
 song = song.replace("luna", "selva")
+
 print(song)
