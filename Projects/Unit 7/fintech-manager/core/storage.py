ed7a671b6595fbc0dab91dca279c9e5a1238a53b
Diego <diegofermend@gmail.com>
2026-07-08 12:29:01 +0200

    unit project

--- Projects/Unit 7/fintech-manager/core/storage.py
+++ Projects/Unit 7/fintech-manager/core/storage.py
@@ -1,8 +1,35 @@
 from pathlib import Path
+import json
 
-main_p = Path(__file__).resolve().parent.parent
+main_path = Path(__file__).resolve().parent.parent
 
-data_path = main_p/"data"
+data_path = main_path/"data"
+transactions_path = data_path/"transactions.json"
+app_log_path = data_path/"app_log.txt"
+config_path = main_path/"config.json"
 
 if not data_path.is_dir():
     data_path.mkdir()
+
+if not transactions_path.exists():
+    with open(transactions_path, "w") as file:
+        transactions_list = []
+        json.dump(transactions_list, file, indent = 2)
+
+if not app_log_path.exists():
+    app_log_path.touch()
+
+try:
+    with open(config_path, "x") as file:
+        initial_data = {"user_name": "name", "currency": "EUR", "expense_limit": 500.00}
+        json.dump(initial_data, file, indent=2)
+except Exception:
+    pass
+
+def load_transactions(file):
+    global transactions_list
+    try:
+        with open(file, "r") as file1:
+            transactions_list = json.load(file1)
+    except Exception:
+        print("Error ocurred loading transactions list")
