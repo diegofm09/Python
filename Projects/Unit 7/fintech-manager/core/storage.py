2f7548a1a0ecf58d2cd7252e64c1aa0197cd9988
Diego <diegofermend@gmail.com>
2026-07-09 12:41:59 +0200

    unit project

--- Projects/Unit 7/fintech-manager/core/storage.py
+++ Projects/Unit 7/fintech-manager/core/storage.py
@@ -9,16 +9,20 @@ app_log_path = data_path/"app_log.txt"
 config_path = main_path/"config.json"
 clean_report_path = data_path/"clean_report.txt"
 
+
 if not data_path.is_dir():
     data_path.mkdir()
+
 
 if not transactions_path.exists():
     with open(transactions_path, "w") as file:
         transactions_list = []
         json.dump(transactions_list, file, indent = 2)
 
+
 if not app_log_path.exists():
     app_log_path.touch()
+
 
 try:
     with open(config_path, "x") as file:
@@ -26,6 +30,7 @@ try:
         json.dump(initial_data, file, indent=2)
 except Exception:
     pass
+
 
 def load_transactions():
     global transactions_list
@@ -37,6 +42,7 @@ def load_transactions():
     finally:
         print("File Closed Safely")
 
+
 def save_transactions():
     global transactions_list
     try:
@@ -47,21 +53,25 @@ def save_transactions():
     finally:
         print("File Closed Safely")
 
+
 def change_expense_limit(new_limit):
     with open(config_path, "r+") as file:
         new_limit_dict = json.load(file)
         current_pointer = file.tell()
         print(f"Current pointer position: {current_pointer}")
         new_limit_dict["expense_limit"] = new_limit
+        
         file.seek(0)
         file.truncate()
         json.dump(new_limit_dict, file, indent=2)
         file.flush()
 
+
 def create_clean_reports(reports_list, file_path):
     with open(file_path, "w") as file3:
         for line in reports_list:
             file3.write(repr(line)+ "\n")
+
 
 def load_app_log(file_path):
     with open(file_path, "r") as file4:
