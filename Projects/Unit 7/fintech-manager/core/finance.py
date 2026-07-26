55dc42a06e5eabebe671ff97786585ec469ed18d
Diego <diegofermend@gmail.com>
2026-07-26 12:21:40 +0200

    unit project

--- Projects/Unit 7/fintech-manager/core/finance.py
+++ Projects/Unit 7/fintech-manager/core/finance.py
@@ -1,4 +1,9 @@
 import random
+from pathlib import Path
+import json
+
+main_path = Path(__file__).resolve().parent.parent
+config_path = main_path/"config.json"
 
 def data_extractor(movement_list, category = None):
         for movement in movement_list:
@@ -57,13 +62,40 @@ def add_international_movements(function_data_extractor_variable):
         yield i
 
 def analyze_transactions(trans_list):
-    analysis = {"net_balance": 0, "highest_income": 0, "highest_spend": 0}
+    analysis = {"net_balance": 0, "highest_income": 0, "highest_expense": 0}
     trans_list_amounts = [i.get("amount") for i in trans_list]
     analysis["net_balance"] = round(sum(trans_list_amounts), 3)
-    analysis["highest_spend"] = min(trans_list_amounts)
+    analysis["highest_expense"] = min(trans_list_amounts)
     analysis["highest_income"] = max(trans_list_amounts)
     return analysis
 
 def filter_transactions(trans_list, minimum_value):
-    filtered_trans_list = list(filter(trans_list, lambda x: x.get("amount")>minimum_value))
-    string_trans_list = list(map(filtered_trans_list, lambda x: f""))
+    with open(config_path, "r") as file:
+        configs = json.load(file)
+    filtered_trans_list = list(filter(lambda x: x.get("amount")>=minimum_value, trans_list))
+    string_trans_list = list(map(lambda x: f"{x.get('concept')}: {x.get('amount')} {configs['currency']}", filtered_trans_list))
+    ids = [i.get("id") for i in filtered_trans_list]
+    ziped_trans_list = list(zip(ids, string_trans_list))
+    return ziped_trans_list
+
+def get_expenses(trans_list):
+    return [i.get("concept").upper() for i in trans_list if i.get("amount")<0]
+
+def get_categorys(trans_list):
+    return {i.get("category") for i in trans_list}
+
+def easy_read(trans_list):
+    return  {i.get("id"): (i.get("category"), i.get("amount")) for i in trans_list}
+
+#mete aqui lo de ordenarlas por fecha
+def sort_transactions(trans_list, sort_key):
+    if sort_key == "money":
+        sorted(trans_list, key = lambda x: x.get("amount"))
+        return trans_list
+    elif sort_key == "date":
+        sorted(trans_list, )
+    else:
+        return trans_list
+    
+def categorys_analysis():
+    pass
