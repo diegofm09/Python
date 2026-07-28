289918a0f10b6d1b392ec928718fa063a8aa5570
Diego <diegofermend@gmail.com>
2026-07-28 13:43:17 +0200

    unit project

--- Projects/Unit 7/fintech-manager/core/utils.py
+++ Projects/Unit 7/fintech-manager/core/utils.py
@@ -1,4 +1,6 @@
 import datetime
+import random
+
 def calculate_compound_interest(initial_money, interest_rate, years):
     assert initial_money>0 and interest_rate>=0 and years>0
     for i in range(years):
@@ -12,3 +14,11 @@ def calendar_planification():
     plus_two_month = (actual_time + one_month*2).strftime("%d/%m/%Y %H:%M:%S")
     plus_three_month = (actual_time + one_month*3).strftime("%d/%m/%Y %H:%M:%S")
     print(f"Today is {actual_time.strftime("%d/%m/%Y %H:%M:%S")}:\n -In 30 days it will be {plus_one_month}\n -In 60 days it will be {plus_two_month}\n -In 90 days it will be {plus_three_month}")
+
+def market_sim(seed_numb=None):
+    if seed_numb:
+        random.seed(seed_numb)
+    stocks = ["AAPL", "NVDA", "GOOG", "MSFT", "AMZN", "AVGO", "META", "SPCX", "TSLA"]
+    while True:
+        variation = round(random.uniform(-4.5, 4.5), 2)
+        yield {"recomended_stock": random.choice(stocks), "variation": variation}
